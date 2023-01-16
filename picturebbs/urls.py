a835068bb5a02a619807d76fe2ed1252386873c8
fuzzy-yusuke <youth_k0228@outlook.com>
2023-01-16 22:33:48 +0900

    詳細画面周りの実装完了

--- picturebbs/urls.py
+++ picturebbs/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name='pypicture'
+
 urlpatterns = [
     #path('admin/', admin.site.urls), #アドミンページのURLです
     path('', views.index, name='index'),
+    path('<int:id>', views.detail, name='detail'),
 ]
--- picturebbs/views.py
+++ picturebbs/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Pictures
 
 def index(request):
-    return render(request, 'pypicture/index.html')
+    pictures = Pictures.objects.all()
+    context = {
+        'message': 'Welcome my PictureBBS',
+        'pictures': pictures,
+    }
+    return render(request, 'pypicture/index.html', context)
+
+def detail(request, id):
+    picture = get_object_or_404(Pictures, pk=id)
+    context = {
+        'message': 'Show MyPicture ' + str(id),
+        'picture': picture,
+    }
+    return render(request, 'pypicture/detail.html', context)
