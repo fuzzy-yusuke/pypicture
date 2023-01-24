2d3568c6a9d7a40e724901578525248e3019ad3f
fuzzy-yusuke <youth_k0228@outlook.com>
2023-01-24 20:14:03 +0900

    検索フォームの追加とbootstrapの適用

--- picturebbs/urls.py
+++ picturebbs/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     #path('admin/', admin.site.urls), #アドミンページのURLです
     path('', views.index, name='index'),
     path('<int:id>', views.detail, name='detail'),
+    path('new',views.new,name='new'),
     path('create', views.create, name='create'),
+    path('<int:id>/edit', views.edit, name='edit'),
+    path('<int:id>/update', views.update, name='update'),
     path('<int:id>/delete', views.delete, name='delete'),
-
 ]
--- picturebbs/views.py
+++ picturebbs/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Pictures
+from .forms import SearchForm
 
 def index(request):
-    pictures = Pictures.objects.all()
+    searchForm = SearchForm(request.GET)
+    if searchForm.is_valid():
+        keyword = searchForm.cleaned_data['keyword']
+        pictures = Pictures.objects.filter(content__contains=keyword)
+    else:
+        searchForm = SearchForm()
+        pictures = Pictures.objects.all()
     context = {
         'message': 'Welcome my PictureBBS',
         'pictures': pictures,
+        'searchForm':searchForm,
     }
     return render(request, 'pypicture/index.html', context)
 
@@ -17,6 +25,9 @@ def detail(request, id):
         'picture': picture,
     }
     return render(request, 'pypicture/detail.html', context)
+
+def new(request):
+    return HttpResponse('this is new.')
 
 def create(request):
     picture = Pictures(content='Hello MYPICTURE', user_name='test')
@@ -29,6 +40,11 @@ def create(request):
     }
     return render(request, 'pypicture/index.html', context)
 
+def edit(request, id):
+    return HttpResponse('this is edit' + str(id))
+
+def update(request, id):
+    return HttpResponse('this is update' + str(id))
 
 def delete(request, id):
     picture = get_object_or_404(Pictures, pk=id)
